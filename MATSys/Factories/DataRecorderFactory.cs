f76f594deab1c60b60e57937c58762e7166327f5
Way Lin <way.lin@jytek.tw>
2022-06-15 00:13:58 +0800

    Apply singleton pattern to keep the configuration

--- MATSys/Factories/DataRecorderFactory.cs
+++ MATSys/Factories/DataRecorderFactory.cs
@@ -1,25 +1,141 @@
-﻿using MATSys.Plugins;
+﻿using System.Reflection;
+using MATSys.Plugins;
 using Microsoft.Extensions.Configuration;
 
 namespace MATSys.Factories
 {
     public sealed class DataRecorderFactory : IDataRecorderFactory
     {
-        private readonly Utility.ModuleContext context;
-        private const string KEY = "DataRecorderFactory";
-        private Type DefaultType { get; } = typeof(EmptyDataRecorder);
-
         public DataRecorderFactory(IConfiguration configuration)
         {
-            context = Utility.ModuleContext.Parse<IDataRecorder>(configuration, KEY);
-            context.TypePrefix = "DataRecorder";
+            DataRecorderContext.GetInstance(configuration);
             //Register assembly resolve event(in case that dynamically loaded assembly had dependent issue)
-            AppDomain.CurrentDomain.AssemblyResolve += context.AssemblyResolve;
+            AppDomain.CurrentDomain.AssemblyResolve += DataRecorderContext.Instance.AssemblyResolve;
         }
 
         public IDataRecorder CreateRecorder(IConfigurationSection section)
         {
-            return context.CreateInstance<IDataRecorder>(section, DefaultType);
+            return DataRecorderContext.Instance.CreateInstance(section);
+        }
+
+
+    }
+    internal sealed class DataRecorderContext
+    {
+        public const string TypePrefix = "DataRecorder";
+        public const string SectionKey = "DataRecorderFactory";
+        public List<Type> Types { get; } = new List<Type>();
+        public IConfiguration? Configuration { get; }
+        public string ModulesFolder { get; } = @".\modules\";
+        public string LibrariesFolder { get; } = @".\libs\";
+        private Type DefaultType { get; } = typeof(EmptyDataRecorder);
+
+        private static Lazy<DataRecorderContext> lazy = new Lazy<DataRecorderContext>();
+
+        public static DataRecorderContext Instance => lazy.Value;
+
+        public static DataRecorderContext GetInstance(IConfiguration config)
+        {
+            if (!lazy.IsValueCreated)
+            {
+                lazy = new Lazy<DataRecorderContext>(() => new DataRecorderContext(config));
+            }
+            return lazy.Value;
+        }
+
+        private DataRecorderContext(IConfiguration config)
+        {
+            Configuration = config;
+
+
+            string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
+
+            //load library folder path from configuration, if not,  use .\libs\
+            var temp = config.GetValue<string>("LibrariesFolder");
+            LibrariesFolder = string.IsNullOrEmpty(temp) ? Path.Combine(baseFolder, "libs") : temp;
+
+            //loadmodules folder path from configuration, if not,  use .\modules\
+            var tempRoot = config.GetValue<string>("ModulesFolder");
+            ModulesFolder = string.IsNullOrEmpty(tempRoot) ? Path.Combine(baseFolder, "modules") : tempRoot;
+
+            //Find all assemblies inherited from IDataRecorder
+            foreach (var item in Directory.GetFiles(ModulesFolder, "*.dll"))
+            {
+                var p = Path.GetFullPath(item);
+                var types = Assembly.LoadFile(p).GetTypes().Where
+                    (x => x.GetInterface(typeof(IDataRecorder).FullName!) != null);
+                Types.AddRange(types);
+            }
+
+        }
+        public Assembly? AssemblyResolve(object? sender, ResolveEventArgs args)
+        {
+            string s = LibrariesFolder + args.Name.Remove(args.Name.IndexOf(',')) + ".dll";
+            if (File.Exists(s))
+            {
+                return Assembly.LoadFile(Path.GetFullPath(s));
+            }
+            else
+            {
+                throw new FileNotFoundException($"Dependent assembly not found : {args.Name}");
+            }
+        }
+        public IDataRecorder CreateInstance(IConfigurationSection section)
+        {
+            IDataRecorder obj;
+
+            //check if section in the json configuration exits
+            if (section.Exists())
+            {
+                var customNameLUT = Configuration?.GetSection(SectionKey);
+
+                Type t;
+
+                string type = section.GetValue<string>("Type");
+                if (string.IsNullOrEmpty(type))
+                {
+                    //if key is empty or null, return immediately with dafault datarecorder object.
+                    return CreateAndLoadInstance(DefaultType, section);
+                }
+
+                //if key has value, search the type with the default class name. eg. xxx=>xxxDataRecorder
+                t = Types.Find(x => x.Name.ToLower() == $"{type}{TypePrefix}".ToLower())!;
+                if (t == null)
+                {
+                    //if searching failed, use the lookup table to get the custom name
+                    var key = customNameLUT.GetValue<string>(type.ToLower());
+                    if (string.IsNullOrEmpty(key))
+                    {
+                        return CreateAndLoadInstance(DefaultType, section);
+                    }
+                    else
+                    {
+                        t = Types.Find(x => x.Name.ToLower() == key)!;
+                    }
+                }
+
+                if (t == null)
+                {
+                    //cannot parse any type, use default datarecorder
+                    return CreateAndLoadInstance(DefaultType, section);
+                }
+                else
+                {
+                    //dynamically load the assembly and object                    
+                    return CreateAndLoadInstance(t, section);
+                }
+            }
+            else
+            {
+                //section is missing, use default DataRecorder object.
+                return CreateAndLoadInstance(DefaultType, section);
+            }
+        }
+        private IDataRecorder CreateAndLoadInstance(Type defaultType, IConfigurationSection section)
+        {
+            var obj = (IDataRecorder)Activator.CreateInstance(defaultType)!;
+            obj.Load(section);
+            return obj;
         }
     }
 }
