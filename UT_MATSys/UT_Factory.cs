2c772faccdd4aa805774fafab95fd1e0cb2c8862
Way Lin <way.lin@jytek.tw>
2022-06-17 13:24:40 +0800

    debug

--- UT_MATSys/UT_Factory.cs
+++ UT_MATSys/UT_Factory.cs
@@ -10,17 +10,28 @@ public class UT_DataRecrderFactory
     [Category("DataRecorder")]
     public void CreateFromFile()
     {
-        var jsonStr = @"{ ""DataRecorder"": {""Type"": ""csv""},
+        try
+        {
+            var jsonStr = @"{ ""DataRecorder"": {""Type"": ""csv""},
         ""ModulesFolder"": ""C:\\Users\\Way-Develop\\MATSys\\UT_MATSys\\bin\\Debug\\net6.0\\modules"",
         }";
-        var ms = new MemoryStream(Encoding.ASCII.GetBytes(jsonStr));
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        var config = cb.AddJsonStream(ms).Build();
-        ms.Close();
-        var fac = new DataRecorderFactory(config);
-        var recorder = fac.CreateRecorder(config.GetSection("DataRecorder"));   
-        recorder.StartServiceAsync(new CancellationToken());     
-        Assert.IsTrue(recorder.Name == "CSVDataRecorder");
+            var ms = new MemoryStream(Encoding.ASCII.GetBytes(jsonStr));
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            var config = cb.AddJsonStream(ms).Build();
+            ms.Close();
+            var fac = new DataRecorderFactory(config);
+            var recorder = fac.CreateRecorder(config.GetSection("DataRecorder"));
+            recorder.StartServiceAsync(new CancellationToken()).Wait();
+            Thread.Sleep(5000);
+            recorder.StopService();
+            
+        }
+        catch   (Exception ex)
+        {
+
+        }
+
+        
     }
 
 }
