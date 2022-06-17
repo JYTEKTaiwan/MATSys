fb6f8eb5c71abcaf3719380fbc6d7e6c8080caa4
Way Lin <way.lin@jytek.tw>
2022-06-17 12:00:18 +0800

    Use global context to generate object

--- MATSys/Factories/CommandServerFactory.cs
+++ MATSys/Factories/CommandServerFactory.cs
@@ -6,73 +6,63 @@ namespace MATSys.Factories
 {
     public sealed class CommandServerFactory : ICommandServerFactory
     {
+        private const string prefix = "CommandServer";
+        private readonly Type DefaultType = typeof(EmptyCommandServer);
+        private readonly DependencyLoader loader;
+        public IEnumerable<Type> Types { get; }
         public CommandServerFactory(IConfiguration configuration)
         {
-            CommandServerContext.Configure(configuration);
-            //Register assembly resolve event(in case that dynamically loaded assembly had dependent issue)            
+            loader = new DependencyLoader(configuration);
+            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;
+            Types = loader.ListTypes<ICommandServer>();
         }
 
         public ICommandServer CreateCommandStream(IConfigurationSection section)
         {
-            return CommandServerContext.Instance.CreateInstance(section);
-        }
-    }
-    internal sealed class CommandServerContext
-    {
-        private const string TypePrefix = "CommandServer";
-        private const string SectionKey = "CommandServerFactory";
-        private Type DefaultType { get; } = typeof(EmptyCommandServer);
-        public List<Type> Types { get; } = new List<Type>();
-        public IConfiguration? Configuration { get; }
-        public string ModulesFolder { get; } = @".\modules\";
-        public string LibrariesFolder { get; } = @".\libs\";
-
-        private static Lazy<CommandServerContext> lazy = new Lazy<CommandServerContext>();
-
-        public static CommandServerContext Instance => lazy.Value;
-
-        public static void Configure(IConfiguration config)
-        {
-            if (!lazy.IsValueCreated)
+            try
             {
-                lazy = new Lazy<CommandServerContext>(() => new CommandServerContext(config));
-            }
-        }
-
-        private CommandServerContext(IConfiguration config)
-        {
-            Configuration = config;
-            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;
-
-            string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
-
-            //load library folder path from configuration, if not,  use .\libs\
-            var temp = config.GetValue<string>("LibrariesFolder");
-            LibrariesFolder = string.IsNullOrEmpty(temp) ? Path.Combine(baseFolder, "libs") : temp;
-
-            //loadmodules folder path from configuration, if not,  use .\modules\
-            var tempRoot = config.GetValue<string>("ModulesFolder");
-            ModulesFolder = string.IsNullOrEmpty(tempRoot) ? Path.Combine(baseFolder, "modules") : tempRoot;
-
-            //Find all assemblies inherited from IDataRecorder
-            foreach (var item in Directory.GetFiles(Path.GetFullPath(ModulesFolder), "*.dll"))
-            {
-                var assem = Assembly.LoadFile(item);
-                //load dependent assemblies
-                foreach (var dependent in assem.GetReferencedAssemblies())
+                //check if section in the json configuration exits
+                if (section.Exists())
                 {
-                    Assembly.Load(dependent);
+
+                    Type t;
+
+                    string type = section.GetValue<string>("Type");
+                    if (string.IsNullOrEmpty(type))
+                    {
+                        //if key is empty or null, return immediately with dafault datarecorder object.
+                        return CreateAndLoadInstance(DefaultType, section);
+                    }
+
+                    //if key has value, search the type with the default class name. eg. xxx=>xxxDataRecorder
+                    t = Types.First(x => x.Name.ToLower() == $"{type}{prefix}".ToLower())!;
+                    if (t == null)
+                    {
+                        //cannot parse any type, use default datarecorder
+                        return CreateAndLoadInstance(DefaultType, section);
+                    }
+                    else
+                    {
+                        //dynamically load the assembly and object                    
+                        return CreateAndLoadInstance(t, section);
+                    }
                 }
-                var types = assem.GetTypes().Where
-                    (x => x.GetInterface(typeof(ICommandServer).FullName!) != null);
-                Types.AddRange(types);
+                else
+                {
+                    //section is missing, use default DataRecorder object.
+                    return CreateAndLoadInstance(DefaultType, section);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
 
         }
         private Assembly? AssemblyResolve(object? sender, ResolveEventArgs args)
         {
             string s1 = args.Name.Remove(args.Name.IndexOf(',')) + ".dll";
-            string s2 = Path.Combine(LibrariesFolder, args.Name.Remove(args.Name.IndexOf(',')) + ".dll");
+            string s2 = Path.Combine(loader.LibraryFolder, args.Name.Remove(args.Name.IndexOf(',')) + ".dll");
             if (File.Exists(s1))
             {
                 return Assembly.LoadFile(Path.GetFullPath(s1));
@@ -83,64 +73,17 @@ namespace MATSys.Factories
             }
             else
             {
-                throw new FileNotFoundException($"Dependent assemaaably not found : {args.Name}");
+                throw new FileLoadException($"Dependent assembly not found : {args.Name}");
             }
         }
-        public ICommandServer CreateInstance(IConfigurationSection section)
-        {
-            //check if section in the json configuration exits
-            if (section.Exists())
-            {
-                var customNameLUT = Configuration?.GetSection(SectionKey);
 
-                Type t;
-
-                string type = section.GetValue<string>("Type");
-                if (string.IsNullOrEmpty(type))
-                {
-                    //if key is empty or null, return immediately with dafault datarecorder object.
-                    return CreateAndLoadInstance(DefaultType, section);
-                }
-
-                //if key has value, search the type with the default class name. eg. xxx=>xxxDataRecorder
-                t = Types.Find(x => x.Name.ToLower() == $"{type}{TypePrefix}".ToLower())!;
-                if (t == null)
-                {
-                    //if searching failed, use the lookup table to get the custom name
-                    var key = customNameLUT.GetValue<string>(type.ToLower());
-                    if (string.IsNullOrEmpty(key))
-                    {
-                        return CreateAndLoadInstance(DefaultType, section);
-                    }
-                    else
-                    {
-                        t = Types.Find(x => x.Name.ToLower() == key)!;
-                    }
-                }
-
-                if (t == null)
-                {
-                    //cannot parse any type, use default datarecorder
-                    return CreateAndLoadInstance(DefaultType, section);
-                }
-                else
-                {
-                    //dynamically load the assembly and object                    
-                    return CreateAndLoadInstance(t, section);
-                }
-            }
-            else
-            {
-                //section is missing, use default DataRecorder object.
-                return CreateAndLoadInstance(DefaultType, section);
-            }
-        }
         private ICommandServer CreateAndLoadInstance(Type defaultType, IConfigurationSection section)
         {
             var obj = (ICommandServer)Activator.CreateInstance(defaultType)!;
             obj.Load(section);
             return obj;
         }
+
     }
 
 }
--- MATSys/Factories/DataBusFactory.cs
+++ MATSys/Factories/DataBusFactory.cs
@@ -6,74 +6,63 @@ namespace MATSys.Factories
 {
     public sealed class DataBusFactory : IDataBusFactory
     {
+        private const string prefix = "DataBus";
+        private readonly Type DefaultType = typeof(EmptyDataBus);
+        private readonly DependencyLoader loader;
+        public IEnumerable<Type> Types { get; }
         public DataBusFactory(IConfiguration configuration)
         {
-            DataBusContext.Configure(configuration);
-            //Register assembly resolve event(in case that dynamically loaded assembly had dependent issue)
-
-        }
-
-        public IDataBus CreatePublisher(IConfigurationSection section)
-        {
-            return DataBusContext.Instance.CreateInstance(section);
-        }
-    }
-
-    internal sealed class DataBusContext
-    {
-        private const string TypePrefix = "DataBus";
-        private const string SectionKey = "DataBusFactory";
-        private Type DefaultType { get; } = typeof(EmptyDataBus);
-        public List<Type> Types { get; } = new List<Type>();
-        public IConfiguration? Configuration { get; }
-        public string ModulesFolder { get; } = @".\modules\";
-        public string LibrariesFolder { get; } = @".\libs\";
-
-        private static Lazy<DataBusContext> lazy = new Lazy<DataBusContext>();
-
-        public static DataBusContext Instance => lazy.Value;
-
-        public static void Configure(IConfiguration config)
-        {
-            if (!lazy.IsValueCreated)
-            {
-                lazy = new Lazy<DataBusContext>(() => new DataBusContext(config));
-            }
-        }
-
-        private DataBusContext(IConfiguration config)
-        {
-            Configuration = config;
+            loader = new DependencyLoader(configuration);
             AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;
-            string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
+            Types = loader.ListTypes<IDataBus>();
+        }
 
-            //load library folder path from configuration, if not,  use .\libs\
-            var temp = config.GetValue<string>("LibrariesFolder");
-            LibrariesFolder = string.IsNullOrEmpty(temp) ? Path.Combine(baseFolder, "libs") : temp;
-
-            //loadmodules folder path from configuration, if not,  use .\modules\
-            var tempRoot = config.GetValue<string>("ModulesFolder");
-            ModulesFolder = string.IsNullOrEmpty(tempRoot) ? Path.Combine(baseFolder, "modules") : tempRoot;
-
-            //Find all assemblies inherited from IDataRecorder
-            foreach (var item in Directory.GetFiles(Path.GetFullPath(ModulesFolder), "*.dll"))
+        public IDataBus CreateDataBus(IConfigurationSection section)
+        {
+            try
             {
-                var assem = Assembly.LoadFile(item);
-                //load dependent assemblies
-                foreach (var dependent in assem.GetReferencedAssemblies())
+                //check if section in the json configuration exits
+                if (section.Exists())
                 {
-                    Assembly.Load(dependent);
+
+                    Type t;
+
+                    string type = section.GetValue<string>("Type");
+                    if (string.IsNullOrEmpty(type))
+                    {
+                        //if key is empty or null, return immediately with dafault datarecorder object.
+                        return CreateAndLoadInstance(DefaultType, section);
+                    }
+
+                    //if key has value, search the type with the default class name. eg. xxx=>xxxDataRecorder
+                    t = Types.First(x => x.Name.ToLower() == $"{type}{prefix}".ToLower())!;
+                    if (t == null)
+                    {
+                        //cannot parse any type, use default datarecorder
+                        return CreateAndLoadInstance(DefaultType, section);
+                    }
+                    else
+                    {
+                        //dynamically load the assembly and object                    
+                        return CreateAndLoadInstance(t, section);
+                    }
                 }
-                var types = assem.GetTypes().Where
-                    (x => x.GetInterface(typeof(IDataBus).FullName!) != null);
-                Types.AddRange(types);
+                else
+                {
+                    //section is missing, use default DataRecorder object.
+                    return CreateAndLoadInstance(DefaultType, section);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
 
         }
         private Assembly? AssemblyResolve(object? sender, ResolveEventArgs args)
         {
             string s1 = args.Name.Remove(args.Name.IndexOf(',')) + ".dll";
-            string s2 = Path.Combine(LibrariesFolder, args.Name.Remove(args.Name.IndexOf(',')) + ".dll");
+            string s2 = Path.Combine(loader.LibraryFolder, args.Name.Remove(args.Name.IndexOf(',')) + ".dll");
             if (File.Exists(s1))
             {
                 return Assembly.LoadFile(Path.GetFullPath(s1));
@@ -84,58 +73,10 @@ namespace MATSys.Factories
             }
             else
             {
-                throw new FileNotFoundException($"Dependent assemaaably not found : {args.Name}");
+                throw new FileLoadException($"Dependent assembly not found : {args.Name}");
             }
         }
-        public IDataBus CreateInstance(IConfigurationSection section)
-        {
-            //check if section in the json configuration exits
-            if (section.Exists())
-            {
-                var customNameLUT = Configuration?.GetSection(SectionKey);
 
-                Type t;
-
-                string type = section.GetValue<string>("Type");
-                if (string.IsNullOrEmpty(type))
-                {
-                    //if key is empty or null, return immediately with dafault datarecorder object.
-                    return CreateAndLoadInstance(DefaultType, section);
-                }
-
-                //if key has value, search the type with the default class name. eg. xxx=>xxxDataRecorder
-                t = Types.Find(x => x.Name.ToLower() == $"{type}{TypePrefix}".ToLower())!;
-                if (t == null)
-                {
-                    //if searching failed, use the lookup table to get the custom name
-                    var key = customNameLUT.GetValue<string>(type.ToLower());
-                    if (string.IsNullOrEmpty(key))
-                    {
-                        return CreateAndLoadInstance(DefaultType, section);
-                    }
-                    else
-                    {
-                        t = Types.Find(x => x.Name.ToLower() == key)!;
-                    }
-                }
-
-                if (t == null)
-                {
-                    //cannot parse any type, use default datarecorder
-                    return CreateAndLoadInstance(DefaultType, section);
-                }
-                else
-                {
-                    //dynamically load the assembly and object                    
-                    return CreateAndLoadInstance(t, section);
-                }
-            }
-            else
-            {
-                //section is missing, use default DataRecorder object.
-                return CreateAndLoadInstance(DefaultType, section);
-            }
-        }
         private IDataBus CreateAndLoadInstance(Type defaultType, IConfigurationSection section)
         {
             var obj = (IDataBus)Activator.CreateInstance(defaultType)!;
@@ -143,5 +84,4 @@ namespace MATSys.Factories
             return obj;
         }
     }
-
 }
