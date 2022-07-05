0ffd8550239ea04287af4b46bb5c93698d88e1c1
Way Lin <way.lin@jytek.tw>
2022-07-05 23:49:16 +0800

    remove the content, add one static method to load the plugins

--- MATSys/GlobalContext.cs
+++ MATSys/GlobalContext.cs
@@ -9,161 +9,23 @@ namespace MATSys;
 
 public class DependencyLoader
 {
-    private readonly IConfiguration? _config;
-    public string ModulesFolder { get; }
-    public DependencyLoader(IConfiguration config)
+    public static void LoadPluginAssemblies(string[] plugins)
     {
-
-#if NETSTANDARD2_0_OR_GREATER
-        AppDomain.CurrentDomain.AssemblyResolve+=CurrentDomain_AssemblyResolve;
-        AppDomain.CurrentDomain.UnhandledException +=CurrentDomain_UnhandledException;
-#endif
-
-        _config = config;
-        var modTemp = config.GetValue<string>("ModulesFolder");
-        bool wrongDirectory= string.IsNullOrEmpty(modTemp)||!Directory.Exists(modTemp);
-        ModulesFolder = wrongDirectory ? DefaultPathInfo.ModulesFolder : modTemp;
-    }
-    public DependencyLoader(string modFolder)
-    {
-        bool wrongDirectory = string.IsNullOrEmpty(modFolder) || !Directory.Exists(modFolder);
-        ModulesFolder = wrongDirectory ? DefaultPathInfo.ModulesFolder : modFolder;
-    }
-
-    private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-    {
-        throw (Exception)e.ExceptionObject;
-    }
-
-    private Assembly? CurrentDomain_AssemblyResolve(object? sender, ResolveEventArgs args)
-    {
-        string s1 = args.Name.Remove(args.Name.IndexOf(',')) + ".dll";
-        string s2 = Path.Combine(ModulesFolder, args.Name.Remove(args.Name.IndexOf(',')) + ".dll");
-        if (File.Exists(s1))
+        foreach (var item in plugins)
         {
-            return Assembly.LoadFile(Path.GetFullPath(s1));
-        }
-        else if (File.Exists(s2))
-        {
-            return Assembly.LoadFile(Path.GetFullPath(s2));
-        }
-        else
-        {
-            throw new FileLoadException($"Assembly not found {args.Name}");
-        }
-    }
-
-    public IEnumerable<Type> ListModuleTypes<T>() where T : IModule
-    {
-        foreach (var item in Directory.GetFiles(Path.GetFullPath(ModulesFolder), "*.dll"))
-        {
+            var p = Path.GetFullPath(item);
 #if NET6_0_OR_GREATER
-            var loader = new PluginLoader(item);
-            var assem = loader.LoadFromAssemblyPath(item);
+            var loader = new PluginLoader(p);
+            loader.LoadFromAssemblyPath(p);
+
 #endif
 #if NETSTANDARD2_0_OR_GREATER
-            var assem = Assembly.LoadFile(item);
+ Assembly.LoadFile(p);
 #endif
-            //load dependent assemblies        
-            var types = assem.GetTypes().Where
-                (x => x.GetInterface(typeof(T).FullName!) != null);
-            foreach (var t in types)
-            {
-                yield return t;
-            }
         }
-    }
-    public IEnumerable<DeviceInformation> ListDevices(IConfiguration? config = null)
-    {
-        var configuration = config == null ? _config : config;
-        if (configuration!.GetSection("Devices").Exists())
-        {
-
-            var pairs = configuration.GetSection("Devices").AsEnumerable(true).Where(x => x.Value != null);
-            foreach (var item in pairs)
-            {
-                var info = Parse(item);
-                if (!string.IsNullOrEmpty(info.Name))
-                {
-                    yield return info;
-                }
-            }
-
-        }
-    }
-
-    private DeviceInformation Parse(KeyValuePair<string, string> kvPair)
-    {
-        var searchKey = kvPair.Key.Contains(':') ? kvPair.Key.Split(':')[0] : kvPair.Key;
-
-        var info = DeviceInformation.Empty;
-
-        bool isModFound = false;
-        if (Directory.Exists(Path.GetFullPath(ModulesFolder)))
-        {
-
-            //Search in the ModulesFolder
-            foreach (var item in Directory.GetFiles(Path.GetFullPath(ModulesFolder), "*.dll"))
-            {
-        #if NET6_0_OR_GREATER
-                    var loader = new PluginLoader(item);
-                    var assem = loader.LoadFromAssemblyPath(item);
-        #endif
-
-        #if NETSTANDARD2_0_OR_GREATER
-                    var assem = Assembly.LoadFile(item);
-        #endif
-
-                var types = assem.GetTypes().Where
-                    (x => x.Name == searchKey && x.GetInterface(typeof(IDevice).FullName!) != null);
-                if (types.Count() > 0)
-                {                    
-                    isModFound = true;
-                    info= new DeviceInformation(types.First(), kvPair.Value);
-                }
-                else
-                {
-                    continue;
-                }
-            }
-
-            //search base directoy if no modules were found is the ModulesFolder
-            if (!isModFound)
-            {
-                info= SearchDeviceInBaseDirectory(searchKey, kvPair.Value);
-            }
-        }
-        else
-        {
-            //ModulesFolder is not exists, search BaseDirectory instead
-            info =SearchDeviceInBaseDirectory(searchKey, kvPair.Value);
-        }
-        return info;
-    }
-    private DeviceInformation SearchDeviceInBaseDirectory(string key, string value)
-    {
-        bool isModFound = false;
-        foreach (var item in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            var types = item.GetTypes().Where
-                (x => x.Name == key && x.GetInterface(typeof(IDevice).FullName!) != null);
-            if (types.Count() > 0)
-            {
-                isModFound = true;
-                return new DeviceInformation(types.First(), value);
-            }
-            else
-            {
-                continue;
-            }
-        }
-        return DeviceInformation.Empty;
-
 
     }
-
 }
-
 #if NET6_0_OR_GREATER
 internal class PluginLoader : AssemblyLoadContext
 {
@@ -202,6 +64,8 @@ public struct DeviceInformation
     public Type DeviceType { get; }
     public string Name { get; }
 
+    public bool IsEmpty => string.IsNullOrEmpty(this.Name);
+
     public DeviceInformation(Type deviceType, string name)
     {
         DeviceType = deviceType;
@@ -209,10 +73,4 @@ public struct DeviceInformation
     }
 
     public static DeviceInformation Empty => new DeviceInformation(null!, "");
-}
-
-
-internal class DefaultPathInfo
-{
-    public static string ModulesFolder { get; } = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"modules");
 }
