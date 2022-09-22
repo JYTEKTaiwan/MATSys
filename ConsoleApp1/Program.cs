e7ac989b06f2b16560f3fe1ea275c46f51fff2f2
Way Lin <way.lin@jytek.tw>
2022-09-22 13:00:14 +0800

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,24 +3,27 @@ using MATSys;
 using MATSys.Commands;
 using MATSys.Factories;
 using MATSys.Plugins;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using System.Security.Policy;
 
-string subFolder = @".\modules";
-if (Directory.Exists(subFolder))
-{
-    DependencyLoader.LoadPluginAssemblies(Directory.GetFiles(subFolder));
-}
 
 
-foreach (var item in MATSysContext.SearchPlugins())
+foreach (var item in MATSysHelper.ListAllPlugins())
 {
     Console.WriteLine($"{item.Category}\t{item.Type}\t{item.AssemblyPath}");
 }
-foreach (var item in MATSysContext.SearchModules())
+foreach (var item in MATSysHelper.ListAllModules())
 {
     Console.WriteLine($"{item.Category}\t{item.Type}\t{item.AssemblyPath}");
+    foreach (var cmds in ModuleHelper.ShowSupportedCommands(item.Type))
+    {
+        Console.WriteLine(cmds);
+
+    }
+
 }
+
 Console.ReadKey();
 
 
@@ -42,107 +45,14 @@ public class Test : ModuleBase
     }
 
     [MATSysCommand]
-    public void Tesrrt()
+    public void Test1()
     {
 
     }
-}
 
-public class MATSysContext
-{
-    public string ModuleAlias { get; set; }
-    public string Category { get; set; }
-    public bool IsExternalAssembly { get; set; }
-    public string AssemblyPath { get; set; }
-    public string Type { get; set; }
-
-    public static IEnumerable<MATSysContext> SearchPlugins()
+    [MATSysCommand("Another")]
+    public void Test2(int a,double b, string c, DateTime d,bool e)
     {
-        List<MATSysContext> list = new List<MATSysContext>();
-        Type t = typeof(IPlugin);
-
-        //search executing assembly
-        foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            foreach (var item in assem.GetTypes())
-            {
-                if (t.IsAssignableFrom(item) && !item.IsAbstract && !item.IsInterface)
-                {
-                    list.Add(new MATSysContext()
-                    {
-                        Type = Parse(item.Name).name,
-                        Category = Parse(item.Name).type,
-                        ModuleAlias = item.Name,
-                        IsExternalAssembly = false,
-                        AssemblyPath = assem.Location
-                    }); ; ;
-                }
-            }
-        }
-        return list;
-
-    }
-
-    public static IEnumerable<MATSysContext> SearchModules()
-    {
-        List<MATSysContext> list = new List<MATSysContext>();
-        Type t = typeof(IModule); 
-        
-
-        //search executing assembly
-        foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
-        {
-            foreach (var item in assem.GetTypes())
-            {
-                if (t.IsAssignableFrom(item) && !item.IsAbstract && !item.IsInterface)
-                {
-                    list.Add(new MATSysContext()
-                    {
-                        ModuleAlias = item.Name,
-                        Type = item.Name,
-                        Category = "Modules",
-                        IsExternalAssembly = false,
-                        AssemblyPath = assem.Location
-                    });
-
-                }
-
-            }
-        }
-
-        return list;
-    }
-
-    private static (string name, string type) Parse(string input)
-    {
-
-        var name = "";
-        var type = "";
-        if (input.Contains("Transceiver"))
-        {
-            name = input.Split("Transceiver")[0];
-            type = "Transceiver";
-        }
-        else if (input.Contains("Notifier"))
-        {
-            name = input.Split("Notifier")[0];
-            type = "Notifier";
-        }
-        else if (input.Contains("Recorder"))
-        {
-            name = input.Split("Recorder")[0];
-            type = "Recorder";
-        }
-        else
-        {
-            name = "";
-            type = "";
-        }
-        name=name.ToLower();
-        name = name == "empty" ? "" : name;
-        return (name, type);
-            
-
 
     }
 
