3fdb76b44edb226c0bed51fd62c56f19d370d6b9
Way Lin <way.lin@jytek.tw>
2022-09-22 12:59:44 +0800

    Add Helper class

--- MATSys/Utilities.cs
+++ MATSys/Utilities.cs
@@ -1,4 +1,5 @@
 using MATSys.Commands;
+using System.Linq;
 using System.Reflection;
 
 #if NET6_0_OR_GREATER
@@ -30,6 +31,149 @@ public class DependencyLoader
 
         }
     }
+}
+
+public class MATSysHelper
+{
+
+    public static IEnumerable<MATSysIServiceContext> ListAllPlugins()
+    {
+        string subFolder = @".\modules";
+        if (Directory.Exists(subFolder))
+        {
+            DependencyLoader.LoadPluginAssemblies(Directory.GetFiles(subFolder));
+        }
+
+        Type t = typeof(IPlugin);
+
+        //search executing assembly
+        foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            foreach (var item in assem.GetTypes())
+            {
+                if (t.IsAssignableFrom(item) && !item.IsAbstract && !item.IsInterface)
+                {
+                    yield return new MATSysIServiceContext()
+                    {
+                        Type = Parse(item.Name).name,
+                        Category = Parse(item.Name).type,
+                        Alias = item.Name,
+                        AssemblyPath = assem.Location
+                    };
+                }
+            }
+        }
+    }
+
+    public static IEnumerable<MATSysIServiceContext> ListAllModules()
+    {
+        string subFolder = @".\modules";
+        if (Directory.Exists(subFolder))
+        {
+            DependencyLoader.LoadPluginAssemblies(Directory.GetFiles(subFolder));
+        }
+
+
+        Type t = typeof(IModule);
+
+        //search executing assembly
+        foreach (var assem in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            foreach (var item in assem.GetTypes())
+            {
+                if (t.IsAssignableFrom(item) && !item.IsAbstract && !item.IsInterface)
+                {
+                    yield return new MATSysIServiceContext()
+                    {
+                        Alias = item.Name,
+                        Type = item.Name,
+                        Category = "Modules",
+                        AssemblyPath = assem.Location
+                    };
+
+                }
+
+            }
+        }
+
+    }
+
+    private static (string name, string type) Parse(string input)
+    {
+
+        var name = "";
+        var type = "";
+        var option = StringSplitOptions.None;
+        if (input.Contains("Transceiver"))
+        {
+            name = input.Split(new string[] { "Transceiver"}, option)[0];
+            type = "Transceiver";
+        }
+        else if (input.Contains("Notifier"))
+        {
+            name = input.Split(new string[] { "Notifier" }, option)[0];
+            type = "Notifier";
+        }
+        else if (input.Contains("Recorder"))
+        {
+            name = input.Split(new string[] { "Recorder" }, option)[0];
+            type = "Recorder";
+        }
+        else
+        {
+            name = "";
+            type = "";
+        }
+        name = name.ToLower();
+        name = name == "empty" ? "" : name;
+        return (name, type);
+
+
+
+    }
+
+}
+
+public class ModuleHelper
+{
+    public static Type? GetModuleType(string typeName)
+    {
+        var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes());
+        return types.FirstOrDefault(x => x.Name == typeName);
+    }
+    public static IEnumerable<string> ShowSupportedCommands(string typeName)
+    {
+        var t = GetModuleType(typeName);
+        return ShowSupportedCommands(t);
+    }
+    public static IEnumerable<string> ShowSupportedCommands<T>() where T : IModule
+    {
+        return ShowSupportedCommands(typeof(T));
+    }
+    public static IEnumerable<string> ShowSupportedCommands(Type t) 
+    {
+        if (typeof(IModule).IsAssignableFrom(t))
+        {
+            var methods = t.GetMethods().Where(x => x.GetCustomAttribute<MATSysCommandAttribute>() != null);
+            foreach (var mi in methods)
+            {
+                var cmd = mi.GetCustomAttribute<MATSysCommandAttribute>();
+                cmd.ConfigureCommandType(mi);
+                yield return cmd.GetTemplateString();
+            }
+        }
+        
+
+    }
+}
+
+public class MATSysIServiceContext
+{
+    public string Alias { get; set; }
+    public string Category { get; set; }
+    public string AssemblyPath { get; set; }
+    public string Type { get; set; }
+
 }
 
 #if NET6_0_OR_GREATER
