9ee6a41a776a398f249457cf96c409ba93643da8
Way Lin <way.lin@jytek.tw>
2022-11-15 14:13:00 +0800

    temp

---
+++ MATSys/Hosting/Analyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MATSys.Hosting
+{
+    public partial class Analyzer
+    {
+        public static bool LargerThan(double result, string input)
+        {
+            return JsonSerializer.Deserialize<double>(input) > result;
+        }
+    }
+}
