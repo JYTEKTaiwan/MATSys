5e5d48298c0040efafc76eb26a969c084dcf2a23
Way Lin <way.lin@jytek.tw>
2023-11-07 16:18:46 +0800

    add webhost example

---
+++ examples/WebConsoleHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using MATSys.Hosting;
+
+#region For Webapplicatio usage
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup=new Startup(builder.Configuration);
+
+startup.ConfigureBuilder(builder);
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app,builder.Environment);
+
+app.Run();
+
+#endregion
---
+++ examples/WebConsoleHost/Startup.cs
@@ -0,0 +1,48 @@
+
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using MATSys.Hosting;
+
+public class Startup
+{
+    public IConfiguration configRoot
+    {
+        get;
+    }
+    public Startup(IConfiguration configuration)
+    {
+        configRoot = configuration;
+    }
+
+    public void ConfigureBuilder(WebApplicationBuilder builder)
+    {
+        File.Delete(Path.Combine(Path.GetTempPath(), "socket.tmp"));
+        var socketPath = Path.Combine(Path.GetTempPath(), "socket.tmp");
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        builder.WebHost.ConfigureKestrel(serverOptions =>
+        {
+            serverOptions.ListenUnixSocket(socketPath, listenOptions =>
+            {
+                listenOptions.Protocols = HttpProtocols.Http2;
+            });
+        });
+
+        builder.Configuration.AddConfigurationInMATSys();
+        builder.Logging.AddNlogInMATSys();
+
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // services.AddCodeFirstGrpc();
+        // services.AddSingleton<MyService>();
+
+        services.AddMATSysService();
+    }
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        // app.MapGrpcService<GreeterService>();
+
+        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+    }
+}
