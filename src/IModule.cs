1af642631fef4e7c9d2f94f76bca16a7c1f088d8
Way Lin <way.lin@jytek.tw>
2023-11-08 11:42:16 +0800

    add event after module is disposed

--- src/IModule.cs
+++ src/IModule.cs
@@ -19,6 +19,8 @@ namespace MATSys
         /// </summary>
         event NewDataReady? OnDataReady;
 
+        event EventHandler IsDisposed;
+
         IConfigurationSection Configuration{get;}
 
         /// <summary>
