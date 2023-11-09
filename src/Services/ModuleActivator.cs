66daf35210d911e2bcae6ef692fcacc92b314e6f
Way Lin <way.lin@jytek.tw>
2023-11-09 10:24:14 +0800

    add comments and logic check

--- src/Services/ModuleActivator.cs
+++ src/Services/ModuleActivator.cs
@@ -1,25 +1,36 @@
-
-
-using System.Runtime.InteropServices;
 using MATSys;
 using MATSys.Factories;
-using Microsoft.CodeAnalysis;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
+/// <summary>
+/// Handler for the modules in the host
+/// </summary>
 public class ModuleActivator
 {
     private readonly IServiceProvider _provider;
-    private readonly Dictionary<string,IConfigurationSection> _modConfigurations;
+    private readonly Dictionary<string, IConfigurationSection> _modConfigurations;
     private readonly IModuleFactory _moduleFactory;
-    private  Dictionary<string, IModule> _modules=new Dictionary<string, IModule>();
-    public int Count=>_modules.Count;
+    private Dictionary<string, IModule> _modules = new Dictionary<string, IModule>();
+    /// <summary>
+    /// Count of the created modules
+    /// </summary>
+    public int Count => _modules.Count;
+    /// <summary>
+    /// ctor
+    /// </summary>
+    /// <param name="provider">service provider from host</param>
     public ModuleActivator(IServiceProvider provider)
     {
-        _provider=provider;
-        _modConfigurations=provider.GetRequiredService<IConfiguration>().GetSection("MATSys:Modules").GetChildren().ToDictionary(x=>x["Alias"]);   
-        _moduleFactory=_provider.GetRequiredService<IModuleFactory>();     
+        _provider = provider;
+        var modSection=provider.GetRequiredService<IConfiguration>().GetSection("MATSys:Modules");
+        if (!modSection.Exists()) throw new ArgumentNullException("MATSys:Modules seciton is not existed in the configuration file");
+        _modConfigurations = modSection.GetChildren().ToDictionary(x => x["Alias"]!);
+        _moduleFactory = _provider.GetRequiredService<IModuleFactory>();
     }
+    /// <summary>
+    /// Create new instance using key specified in the configuration file
+    /// </summary>
+    /// <param name="key">Alias property in the configuration file</param>
+    /// <returns>IModule instance</returns>
     public IModule Create(string key)
     {
         if (_modules.ContainsKey(key))
@@ -28,17 +39,17 @@ public class ModuleActivator
         }
         else
         {
-            var mod=_moduleFactory.CreateModule(_modConfigurations[key]);
-            mod.IsDisposed +=ModuleDisposed;
-            _modules.Add(key,mod);
+            var mod = _moduleFactory.CreateModule(_modConfigurations[key]);
+            mod.IsDisposed += ModuleDisposed;
+            _modules.Add(key, mod);
             return mod;
-        }        
+        }
     }
 
-    private  void ModuleDisposed(object sender, EventArgs e)
+    private void ModuleDisposed(object? sender, EventArgs e)
     {
-        _modules.Remove(((IModule)sender).Alias);
+        if (sender != null) _modules.Remove(((IModule)sender).Alias);
     }
-    
+
 
 }
