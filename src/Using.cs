d4cd2b1ea438c86aed549a4c0d7f3a0664508589
Way Lin <way.lin@jytek.tw>
2023-11-09 09:42:21 +0800

    add global using

---
+++ src/Using.cs
@@ -0,0 +1,10 @@
+﻿global using System.Data;
+global using System.Reflection;
+global using System.Runtime.CompilerServices;
+global using System.Text;
+global using System.Text.Json.Nodes;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using NLog;
+global using NLog.Extensions.Logging;
