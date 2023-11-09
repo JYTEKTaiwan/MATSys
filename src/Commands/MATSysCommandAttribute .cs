0d53673ed81a3e11b3c2826e2c08dccd25cdcaaa
Way Lin <way.lin@jytek.tw>
2023-11-09 09:52:05 +0800

    add comments

--- src/Commands/MATSysCommandAttribute .cs
+++ src/Commands/MATSysCommandAttribute .cs
@@ -129,23 +129,44 @@ namespace MATSys.Commands
 
     }
 
+    /// <summary>
+    /// Attributes that indicate the class can be converted to ICommand
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public sealed class MATSysCommandContractAttribute : Attribute
     {
+        /// <summary>
+        /// Method Name that used in ICommand
+        /// </summary>        
         public string MethodName { get; }
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="name">name of the method in ICommand</param> 
         public MATSysCommandContractAttribute(string name)
         {
-            
+
             MethodName = name;
         }
     }
-     [AttributeUsage(AttributeTargets.Property)]
+    /// <summary>
+    /// Attributes that indicate the property of the instance will be used to convert as parameter in ICommand
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
     public sealed class MATSysCommandOrderAttribute : Attribute
     {
-        public int Order{get;}
+
+        /// <summary>
+        /// Order used to create ICommand
+        /// </summary>        
+        public int Order { get; }
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="order">order of the property</param>
         public MATSysCommandOrderAttribute(int order)
         {
-            Order=order;
+            Order = order;
         }
     }
 
