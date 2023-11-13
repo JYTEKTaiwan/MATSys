51bdeb4af84035bb4a1e8b5eafd6ea91754c8546
Way Lin <way.lin@jytek.tw>
2023-11-13 10:40:49 +0800

    class to save the method info that marked with attribute

---
+++ src/Commands/MATSysCommandContext.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using MATSys.Commands;
+
+namespace MATSys;
+
+internal class MATSysContext
+{
+    public string MethodName { get; }
+    public MethodInvoker Invoker { get; }
+    public Type CommandType { get; }
+
+    internal MATSysContext(object target, MethodInfo mi)
+    {
+        var cmd = mi.GetCustomAttribute<MATSysCommandAttribute>()!;
+        MethodName = cmd.Alias;
+        Invoker = MethodInvoker.Create(target,mi);
+
+
+        var types = mi.GetParameters().Select(x => x.ParameterType).ToArray();
+        Type t = GetGenericCommandType(types.Length);
+        if (t.IsGenericType)
+        {
+            CommandType = t.MakeGenericType(types);
+        }
+        else
+        {
+            CommandType = t;
+        }
+
+
+    }
+
+    public static MATSysContext Create(object target, MethodInfo mi)
+    {
+        return new MATSysContext(target, mi);
+    }
+
+    private Type GetGenericCommandType(int count)
+    {
+        switch (count)
+        {
+            case 0:
+                return typeof(Command);
+            case 1:
+                return typeof(Command<>);
+            case 2:
+                return typeof(Command<,>);
+            case 3:
+                return typeof(Command<,,>);
+            case 4:
+                return typeof(Command<,,,>);
+            case 5:
+                return typeof(Command<,,,,>);
+            case 6:
+                return typeof(Command<,,,,,>);
+            case 7:
+                return typeof(Command<,,,,,,>);
+            default:
+                return typeof(Command);
+        }
+
+    }
+
+}
