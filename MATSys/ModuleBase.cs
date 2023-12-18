18ba81054a17e433e95c8b941cca195ba629e522
Way Lin <way.lin@jytek.tw>
2023-12-18 08:57:24 +0800

    code cleanup

--- MATSys/ModuleBase.cs
+++ MATSys/ModuleBase.cs
@@ -14,20 +14,6 @@ namespace MATSys
     public abstract class ModuleBase : IModule
     {
         #region Private Fields
-
-        /// <summary>
-        /// section key for recorder in specific section of module
-        /// </summary>
-        private const string key_recorder = "Recorder";
-
-        /// <summary>
-        /// section key for notifier in specific section of module
-        /// </summary>
-        private const string key_notifier = "Notifier";
-        /// <summary>
-        /// section key for transceiver in specific section of module
-        /// </summary>
-        private const string key_transceiver = "Transceiver";
         private NLog.ILogger? _logger;
         private ITransceiver _transceiver = new EmptyTransceiver();
         private IRecorder _recorder = new EmptyRecorder();
@@ -39,10 +25,9 @@ namespace MATSys
 #elif NET6_0_OR_GREATER || NETSTANDARD2_0
         private ReadOnlyDictionary<string, MATSysContext> cmds = null!;
 #else
-        private Dictionary<string, MATSysContext> cmds = null!;
+        private Dictionary<string, MATSysContext> cmds = null;
 #endif
 
-        private IConfigurationSection? _config;
         #endregion
 
         #region Public Properties
@@ -86,8 +71,20 @@ namespace MATSys
         /// </summary>
         public event EventHandler? IsDisposed;
 
+
+        /* Unmerged change from project 'MATSys (net35)'
+        Before:
+                #endregion
+
+                #region Constructor
+        After:
+                #endregion
+
+                #region Constructor
+        */
         #endregion
 
+        #region Constructor
         protected ModuleBase()
         {
 #if NET8_0_OR_GREATER
@@ -100,11 +97,9 @@ namespace MATSys
             _logger = LogManager.GetCurrentClassLogger();
 
         }
+        #endregion
 
         #region Public Methods
-
-
-
 
 #if NET6_0_OR_GREATER || NETSTANDARD2_0
         /// <summary>
@@ -147,23 +142,6 @@ namespace MATSys
                 }
             });
         }
-        /// <summary>
-        /// Execute the incoming command object
-        /// </summary>
-        /// <param name="cmd">ICommand instance</param>
-        /// <returns>reponse after executing the commnad</returns>
-        public string Execute(ICommand cmd)
-        {
-            try
-            {
-                return ExecuteAsync(cmd).Result;
-            }
-            catch (AggregateException ex)
-            {
-                _logger?.Warn(ex.Message);
-                return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_execError, ex, cmd);
-            }
-        }
         public async Task<string> ExecuteAsync(string methodName, params object[] parameters)
         {
             return await Task.Run(async () =>
@@ -187,6 +165,11 @@ namespace MATSys
                     _logger?.Warn(ex);
                     return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_notFound, ex, methodName);
                 }
+                catch (AggregateException ex)
+                {
+                    _logger?.Warn(ex.Message);
+                    return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_execError, ex, methodName);
+                }
                 catch (System.Text.Json.JsonException ex)
                 {
                     _logger?.Warn(ex);
@@ -200,26 +183,149 @@ namespace MATSys
             });
 
         }
-        public string Execute(string methodName, params object[] parameters)
-        {
-            try
-            {
-                return ExecuteAsync(methodName,parameters).Result;
-            }
-            catch (AggregateException ex)
-            {
-                _logger?.Warn(ex.Message);
-                return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_execError, ex, methodName);
-            }
-        }
-#elif NET35
-
+#endif
         /// <summary>
         /// Execute the incoming command object
         /// </summary>
         /// <param name="cmd">ICommand instance</param>
         /// <returns>reponse after executing the commnad</returns>
         public string Execute(ICommand cmd)
+        {
+#if NET6_0_OR_GREATER || NETSTANDARD2_0
+            return ExecuteAsync(cmd).Result;
+#elif NET35
+            return Execute_Net35(cmd);
+#endif
+        }
+        public string Execute(string methodName, params object[] parameters)
+        {
+#if NET6_0_OR_GREATER || NETSTANDARD2_0
+            return ExecuteAsync(methodName, parameters).Result;
+#elif NET35
+            return Execute_Net35(methodName, parameters);
+#endif
+        }
+        public string ExecuteCommandString(string cmdInJson) => OnRequestReceived(this, cmdInJson);
+
+        /// <summary>
+        /// List all methods in simplied string format 
+        /// </summary>
+        /// <returns>collection of string</returns>
+        public virtual IEnumerable<string> PrintCommands()
+        {
+            foreach (var item in cmds.Values)
+            {
+
+#if NET6_0_OR_GREATER || NETSTANDARD2_0
+                var args = item.CommandType!.GenericTypeArguments;
+                System.Text.Json.Nodes.JsonArray arr = new System.Text.Json.Nodes.JsonArray();
+                for (int i = 0; i < args.Length; i++)
+                {
+                    arr.Add(args[i].Name);
+                }
+                System.Text.Json.Nodes.JsonObject jobj = new System.Text.Json.Nodes.JsonObject();
+                jobj.Add(item.MethodName, arr);
+                yield return jobj.ToJsonString();
+#elif NET35
+                var args = item.CommandType!.GetGenericArguments();
+                Newtonsoft.Json.Linq.JArray arr = new Newtonsoft.Json.Linq.JArray();
+                for (int i = 0; i < args.Length; i++)
+                {
+                    arr.Add(args[i].Name);
+                }
+                Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
+                jobj.Add(item.MethodName, arr);
+                yield return jobj.ToString();
+#endif
+            }
+
+        }
+#if NET6_0_OR_GREATER || NETSTANDARD2_0
+        /// <summary>
+        /// Export the ModuleBase instance to json context
+        /// </summary>
+        /// <returns></returns>
+        public System.Text.Json.Nodes.JsonObject Export()
+        {
+            System.Text.Json.Nodes.JsonObject jObj = new System.Text.Json.Nodes.JsonObject();
+
+            jObj.Add("Name", Alias);
+            jObj.Add("Type", this.GetType().Name);
+            if (_recorder.Export().Count > 0) jObj.Add("Recorder", _recorder.Export());
+
+            if (_notifier.Export().Count > 0) jObj.Add("Notifier", _notifier.Export());
+
+            if (_transceiver.Export().Count > 0) jObj.Add("Transceiver", _transceiver.Export());
+
+            return jObj;
+        }
+
+#elif NET35
+        /// <summary>
+        /// Export the ModuleBase instance to json context
+        /// </summary>
+        /// <returns></returns>
+        public Newtonsoft.Json.Linq.JObject Export()
+        {
+            Newtonsoft.Json.Linq.JObject jObj = new Newtonsoft.Json.Linq.JObject();
+
+            jObj.Add("Name", Alias);
+            jObj.Add("Type", this.GetType().Name);
+
+            if (_recorder.Export().Count > 0) jObj.Add("Recorder", _recorder.Export());
+
+            if (_notifier.Export().Count > 0) jObj.Add("Notifier", _notifier.Export());
+
+            if (_transceiver.Export().Count > 0) jObj.Add("Transceiver", _transceiver.Export());
+
+            return jObj;
+        }
+
+#endif
+        /// <summary>
+        /// Export ModuleBase instance to json string format
+        /// </summary>
+        /// <param name="indented"></param>
+        /// <returns></returns>
+        public string Export(bool indented = true)
+        {
+#if NET6_0_OR_GREATER || NETSTANDARD2_0
+            return Export().ToJsonString(new System.Text.Json.JsonSerializerOptions() { WriteIndented = indented });
+#elif NET35
+            if (indented) return Export().ToString(Newtonsoft.Json.Formatting.Indented);
+            else return Export().ToString(Newtonsoft.Json.Formatting.None);
+
+#else
+#endif
+        }
+        /// <summary>
+        /// Configurae IModule instance with object 
+        /// </summary>
+        /// <param name="option">configuration object</param>
+        public virtual void Configure(object? option) { }
+        /// <summary>
+        /// Inject the IRecorder instance into IModule instance
+        /// </summary>
+        /// <param name="recorder">IRecorder instance</param>
+        public void InjectPlugin(IRecorder? recorder) => _recorder = recorder == null ? _recorder : recorder;
+
+        /// <summary>
+        /// Inject the ITransceiver instance into IModule instance
+        /// </summary>
+        /// <param name="transceiver">ITransceiver instance</param>
+        public void InjectPlugin(ITransceiver? transceiver) => _transceiver = transceiver == null ? _transceiver : transceiver;
+
+        /// <summary>
+        /// Inject the INotifier instance into IModule instance
+        /// </summary>
+        /// <param name="notifier">INotifier instance </param>
+        public void InjectPlugin(INotifier? notifier) => _notifier = notifier == null ? _notifier : notifier;
+
+        #endregion
+
+        #region Private methods
+#if NET35
+        private string Execute_Net35(ICommand cmd)
         {
             try
             {
@@ -245,204 +351,46 @@ namespace MATSys
                 _logger?.Warn(ex);
                 return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_execError, ex, cmd);
             }
+
         }
-        
-        
-        public string Execute(string methodName, params object[] parameters)
+        private string Execute_Net35(string methodName, params object[] parameters)
         {
-            var invoker = cmds[methodName].Invoker;
-            if (invoker == null)
+            try
             {
-                throw new NullReferenceException("invoker is null");
-            }
-            var result = invoker.Invoke(parameters);
-            var response = Utilities.Serializer.Serialize(result, false)!;
-            return response!;
-        }
-
-
-#endif
-        public string ExecuteCommandString(string cmdInJson)
-        {
-            return OnRequestReceived(this, cmdInJson);
-        }
-
-
-        /// <summary>
-        /// List all methods in simplied string format 
-        /// </summary>
-        /// <returns>collection of string</returns>
-        public virtual IEnumerable<string> PrintCommands()
-        {
-#if NET6_0_OR_GREATER || NETSTANDARD2_0
-            foreach (var item in cmds.Values)
-            {
-                var args = item.CommandType!.GenericTypeArguments;
-                System.Text.Json.Nodes.JsonArray arr = new System.Text.Json.Nodes.JsonArray();
-                for (int i = 0; i < args.Length; i++)
+                var invoker = cmds[methodName].Invoker;
+                if (invoker == null)
                 {
-                    arr.Add(args[i].Name);
+                    throw new NullReferenceException("invoker is null");
                 }
-                System.Text.Json.Nodes.JsonObject jobj = new System.Text.Json.Nodes.JsonObject();
-                jobj.Add(item.MethodName, arr);
-                yield return jobj.ToJsonString();
+                var result = invoker.Invoke(parameters);
+                var response = Utilities.Serializer.Serialize(result, false)!;
+                return response!;
             }
-#elif NET35
-            foreach (var item in cmds.Values)
+            catch (KeyNotFoundException ex)
             {
-                var args = item.CommandType!.GetGenericArguments();
-                Newtonsoft.Json.Linq.JArray arr = new Newtonsoft.Json.Linq.JArray();
-                for (int i = 0; i < args.Length; i++)
-                {
-                    arr.Add(args[i].Name);
-                }
-                Newtonsoft.Json.Linq.JObject jobj = new Newtonsoft.Json.Linq.JObject();
-                jobj.Add(item.MethodName, arr);
-                yield return jobj.ToString();
+                _logger?.Warn(ex);
+                return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_notFound, ex, methodName);
             }
-#endif
+            catch (Exception ex)
+            {
+                _logger?.Warn(ex);
+                return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_execError, ex, methodName);
+            }
 
-        }
-#if NET6_0_OR_GREATER||NETSTANDARD2_0
-        /// <summary>
-        /// Export the ModuleBase instance to json context
-        /// </summary>
-        /// <returns></returns>
-        public System.Text.Json.Nodes.JsonObject Export()
-        {
-            System.Text.Json.Nodes.JsonObject jObj = new System.Text.Json.Nodes.JsonObject();
 
-            jObj.Add("Name", Alias);
-            jObj.Add("Type", this.GetType().Name);
-            var node = new System.Text.Json.Nodes.JsonObject();
-            node = _recorder.Export();
-            if (node.Count > 0)
-            {
-                jObj.Add("Recorder", _recorder.Export());
-            }
-            node = _notifier.Export();
-            if (node.Count > 0)
-            {
-                jObj.Add("Notifier", _notifier.Export());
-            }
-            node = _transceiver.Export();
-            if (node.Count > 0)
-            {
-                jObj.Add("Transceiver", _transceiver.Export());
-            }
-            return jObj;
-        }
-
-#elif NET35
-        /// <summary>
-        /// Export the ModuleBase instance to json context
-        /// </summary>
-        /// <returns></returns>
-        public Newtonsoft.Json.Linq.JObject Export()
-        {
-            Newtonsoft.Json.Linq.JObject jObj = new Newtonsoft.Json.Linq.JObject();
-
-            jObj.Add("Name", Alias);
-            jObj.Add("Type", this.GetType().Name);
-            var node = new Newtonsoft.Json.Linq.JObject();
-            node = _recorder.Export();
-            if (node.Count > 0)
-            {
-                jObj.Add("Recorder", _recorder.Export());
-            }
-            node = _notifier.Export();
-            if (node.Count > 0)
-            {
-                jObj.Add("Notifier", _notifier.Export());
-            }
-            node = _transceiver.Export();
-            if (node.Count > 0)
-            {
-                jObj.Add("Transceiver", _transceiver.Export());
-            }
-            return jObj;
         }
 
 #endif
-        /// <summary>
-        /// Export ModuleBase instance to json string format
-        /// </summary>
-        /// <param name="indented"></param>
-        /// <returns></returns>
-        public string Export(bool indented = true)
-        {
-#if NET6_0_OR_GREATER || NETSTANDARD2_0
-            return Export().ToJsonString(new System.Text.Json.JsonSerializerOptions() { WriteIndented = indented });
-#elif NET35
-            if (indented) return Export().ToString(Newtonsoft.Json.Formatting.Indented);
-            else return Export().ToString(Newtonsoft.Json.Formatting.None);
-
-#else
-#endif
-        }
-        /// <summary>
-        /// Configurae IModule instance with object 
-        /// </summary>
-        /// <param name="option">configuration object</param>
-        public virtual void Configure(object? option)
-        {
-            
-        }
-        /// <summary>
-        /// Inject the IRecorder instance into IModule instance
-        /// </summary>
-        /// <param name="recorder">IRecorder instance</param>
-        public void InjectPlugin(IRecorder? recorder)
-        {
-            _recorder = recorder!;
-        }
-        /// <summary>
-        /// Inject the ITransceiver instance into IModule instance
-        /// </summary>
-        /// <param name="transceiver">ITransceiver instance</param>
-        public void InjectPlugin(ITransceiver? transceiver)
-        {
-            _transceiver = transceiver!;
-
-
-        }
-        /// <summary>
-        /// Inject the INotifier instance into IModule instance
-        /// </summary>
-        /// <param name="notifier">INotifier instance </param>
-        public void InjectPlugin(INotifier? notifier)
-        {
-            _notifier = notifier!;
-
-        }
-
-        #endregion
-
-        #region Private methods
-
         private Dictionary<string, MATSysContext> ListMATSysCommands()
         {
-#if NET6_0_OR_GREATER || NETSTANDARD2_0
+
             return GetType().GetMethods()
-               .Where(x => x.GetCustomAttributes<MATSysCommandAttribute>().Count() > 0)
+               .Where(x => x.GetCustomAttributes(typeof(MATSysCommandAttribute), false).Count() > 0)
                .Select(x =>
                {
                    return MATSysContext.Create(this, x);
                }).ToDictionary(x => x.MethodName);
-
-#elif NET35
-            return GetType().GetMethods()
-                           .Where(x => x.GetCustomAttributes(typeof(MATSysCommandAttribute), false).Count() > 0)
-                           .Select(x =>
-                           {
-                               return MATSysContext.Create(this, x);
-                           }).ToDictionary(x => x.MethodName);
-
-#endif
-
-            ;
         }
-#if NET6_0_OR_GREATER || NETSTANDARD2_0
         /// <summary>
         /// Event when new request is received
         /// </summary>
@@ -454,42 +402,18 @@ namespace MATSys
             try
             {
                 _logger?.Trace($"Command received: {commandObjectInJson}");
+#if NET6_0_OR_GREATER || NETSTANDARD2_0
                 var sp = commandObjectInJson.AsSpan();
                 var end = sp.IndexOf(':');
                 var start = sp.IndexOf('\"');
                 var item = cmds[sp.Slice(start + 1, end - start - 2).ToString()];
-                var cmd = CommandBase.Deserialize(commandObjectInJson, item.CommandType!);
-                return Execute(cmd);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                _logger?.Warn(ex);
-                return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_notFound, ex, commandObjectInJson);
-            }
-            catch (Exception ex)
-            {
-                _logger?.Error(ex);
-                return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_execError, ex, commandObjectInJson);
-            }
-        }
-
-
 #elif NET35
-        /// <summary>
-        /// Event when new request is received
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="commandObjectInJson"></param>
-        /// <returns></returns>
-        private string OnRequestReceived(object sender, string commandObjectInJson)
-        {
-            try
-            {
-                _logger?.Trace($"Command received: {commandObjectInJson}");
                 var sp = commandObjectInJson;
                 var end = sp.IndexOf(':');
                 var start = sp.IndexOf('\"');
                 var item = cmds[sp.Substring(start + 1, end - start - 2).ToString()];
+#endif
+
                 var cmd = CommandBase.Deserialize(commandObjectInJson, item.CommandType!);
                 return Execute(cmd);
             }
@@ -504,8 +428,6 @@ namespace MATSys
                 return ExceptionHandler.PrintMessage(ExceptionHandler.cmd_execError, ex, commandObjectInJson);
             }
         }
-
-#endif
 
         /// <summary>
         /// Dispose the instance and call GC
@@ -523,11 +445,4 @@ namespace MATSys
         #endregion
     }
 
-    internal interface IConfigurationSection
-    {
-    }
-}
-namespace System.Runtime.CompilerServices
-{
-    public class ExtensionAttribute : Attribute { }
 }
