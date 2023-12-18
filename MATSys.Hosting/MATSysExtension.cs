39e9b7ae1f19b7c56d70034ef052c87c60a7996f
Way Lin <way.lin@jytek.tw>
2023-12-18 16:29:16 +0800

    simplify the code

--- MATSys.Hosting/MATSysExtension.cs
+++ MATSys.Hosting/MATSysExtension.cs
@@ -19,15 +19,12 @@ namespace MATSys.Hosting
         /// <param name="services">service collection from host builder</param>
         /// <returns>IServiceCollection</returns>
         public static IServiceCollection AddMATSysService(this IServiceCollection services)
-        {
-            services.AddSingleton<IRecorderFactory, RecorderFactory>()
-                       .AddSingleton<INotifierFactory, NotifierFactory>()
-                       .AddSingleton<ITransceiverFactory, TransceiverFactory>()
-                        .AddSingleton<IModuleFactory, ModuleFactory>()
-                        .AddSingleton<ModuleActivatorService>();
+        => services.AddSingleton<IRecorderFactory, RecorderFactory>()
+                   .AddSingleton<INotifierFactory, NotifierFactory>()
+                   .AddSingleton<ITransceiverFactory, TransceiverFactory>()
+                   .AddSingleton<IModuleFactory, ModuleFactory>()
+                   .AddSingleton<ModuleActivatorService>();
 
-            return services;
-        }
         /// <summary>
         /// Inject NLog service into logger builder in host
         /// </summary>
@@ -41,7 +38,7 @@ namespace MATSys.Hosting
         /// <param name="configBuilder">configuration builder </param>
         /// <returns>IConfigurationBuilder</returns>
         public static IConfigurationBuilder AddConfigurationInMATSys(this IConfigurationBuilder configBuilder)
-        {
+        {            
             var config = new ConfigurationBuilder()
             //.AddEnvironmentVariables()
             .AddJsonFile("appsettings.json", optional: true)
@@ -56,9 +53,8 @@ namespace MATSys.Hosting
         /// <param name="provider"></param>
         /// <returns></returns>
         public static IConfiguration GetConfigurationRoot(this IServiceProvider provider)
-        {
-            return provider.GetRequiredService<IConfiguration>();
-        }
+        => provider.GetRequiredService<IConfiguration>();
+        
         /// <summary>
         /// Get the specified section of the configuration in the host
         /// </summary>
@@ -66,18 +62,16 @@ namespace MATSys.Hosting
         /// <param name="sectionKey"></param>
         /// <returns></returns>
         public static IConfigurationSection GetConfigurationSection(this IServiceProvider provider, string sectionKey)
-        {
-            return provider.GetRequiredService<IConfiguration>().GetSection(sectionKey);
-        }
+        => provider.GetRequiredService<IConfiguration>().GetSection(sectionKey);
+        
         /// <summary>
         /// Get the collection of information for Module in the host
         /// </summary>
         /// <param name="provider"></param>
         /// <returns></returns>
         public static Dictionary<string, IConfigurationSection> GetAllModuleInfos(this IServiceProvider provider)
-        {
-            return provider.GetConfigurationSection("MATSys:Modules").GetChildren().ToDictionary(x => x["Alias"]!);
-        }
+        => provider.GetConfigurationSection("MATSys:Modules").GetChildren().ToDictionary(x => x["Alias"]!);
+        
 
         /// <summary>
         /// Get the module from the host using alias name 
