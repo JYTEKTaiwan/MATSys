1a3ef960aa836cc9bf10da61569b16bf9d3ac6b7
Way Lin <way.lin@jytek.tw>
2024-04-02 17:51:23 +0800

    update example

--- examples/ConsoleHostNet47/Program.cs
+++ examples/ConsoleHostNet47/Program.cs
@@ -2,65 +2,49 @@
 using MATSys.Commands;
 using MATSys.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading;
+using MATSys.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+using MATSys.Factories;
 
 namespace ConsoleHostNet47
 {
     internal class Program
     {
-        public delegate IModule ModuleResolver(IConfigurationSection section);
+        private delegate IModule ModuleInstanceMapper(string key);
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             var b = Host.CreateDefaultBuilder();
 
             b.ConfigureHostConfiguration(h => h.AddConfigurationInMATSys());
-            b.ConfigureServices(s => s.AddMATSysService());
+            b.ConfigureServices(s =>
+            s.AddMATSysService()
+            );
 
             var host = b.Build();
             host.StartAsync();
 
             Thread.Sleep(1000);
-
             var dev = host.Services.GetModule("Dev1");
-            var mod = host.Services.GetModule("Mod1");
-
+            var dev2 = host.Services.GetModule("Mod1");
+            Console.WriteLine($"{dev.Alias}_{dev.GetHashCode()}");
+            Console.WriteLine($"{dev2.Alias}_{dev2.GetHashCode()}");
             dev.Execute("Delay", 1000);
             host.StopAsync();
-
+            host.Dispose();
             Console.WriteLine("DONE");
+            Console.ReadKey();
         }
     }
-    internal class TestDevice : ModuleBase
-    {
-
-        [MATSysCommand]
-        public void Delay(int a)
-        {
-            Console.WriteLine($"[{DateTime.Now}]Start - {a}");
-            Thread.Sleep(a);
-            Console.WriteLine($"[{DateTime.Now}]Done - {a}");
-        }
-
-
-    }
-
-    internal class TestDevice2 : ModuleBase
-    {
-
-        [MATSysCommand]
-        public void Delay(int a)
-        {
-            Console.WriteLine($"[{DateTime.Now}]Start - {a}");
-            Thread.Sleep(a);
-            Console.WriteLine($"[{DateTime.Now}]Done - {a}");
-        }
-
-
-    }
-
 
 
 }
