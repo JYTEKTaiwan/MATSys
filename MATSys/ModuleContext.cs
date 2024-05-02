a6dd91ecf720480e3b59b206adcbafe248bc3e35
Way Lin <way.lin@jytek.tw>
2024-05-02 16:08:40 +0800

    add toolset to parse the information of module without instantiate it

---
+++ MATSys/ModuleContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MATSys.Commands;
+
+namespace MATSys
+{
+    public sealed class ModuleContext
+    {
+        public string TypeName { get; }
+        public IEnumerable<string> SupportedMethods { get; }
+        private ModuleContext(Type t)
+        {
+            SupportedMethods = t.GetMethods()
+                .Where(x => x.GetCustomAttributes(typeof(MATSysCommandAttribute), true).Count() > 0)
+                .Select(m=>m.ToString());
+            TypeName = t.Name;
+        }
+
+        public static ModuleContext Parse(Type modType)
+        {
+            if (typeof(IModule).IsAssignableFrom(modType))
+            {
+                return new ModuleContext(modType);
+            }
+            else
+            {
+                throw new NotSupportedException($"Type named {modType} is not supported");
+            }
+
+        }
+        public static ModuleContext Parse<T>() where T:IModule
+        {
+            return Parse(typeof(T));
+        }
+
+    }
+
+}
